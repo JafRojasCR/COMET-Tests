bbf199d695aac5b62124684d75e6b82fc5cce871
JafRojasCR <jafetalonsorb08@gmail.com>
2025-08-03 13:18:56 -0600

    Turned application into a PWA

---
+++ public/service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'comet-cache-v1';
+const urlsToCache = [
+  '/', // o tu start_url
+  '/index.html',
+  '/manifest.json', // Asegúrate de tener un manifest.json
+];
+// Evento 'install': se dispara cuando el service worker es instalado
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caché abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Evento 'fetch': intercepta peticiones de la red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve el recurso de la caché si está disponible
+        if (response) {
+          return response;
+        }
+        // Si no está en la caché, continúa con la petición de red
+        return fetch(event.request);
+      })
+  );
+});
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 const path = require('path');
+const express = require('express');
+
+// Ruta para servir archivos estáticos
+
+router.use(express.static(path.join(__dirname, '../public')));
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html")); // Envía el archivo index.html
